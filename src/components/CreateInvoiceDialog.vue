f3fa77307cf257b62099e138882b5141e01facd1
a1denvalu3 <cloudsupper@proton.me>
2026-08-18 23:01:16 +0200

    fix: show deposit limits after address creation

--- src/components/CreateInvoiceDialog.vue
+++ src/components/CreateInvoiceDialog.vue
@@ -75,15 +75,6 @@
           </div>
         </div>
 
-        <div v-if="isOnchain" class="row justify-center q-px-lg q-mb-sm">
-          <div class="col-12 col-sm-11 col-md-8" style="max-width: 600px">
-            <OnchainDepositLimits
-              :mint-url="activeMintUrl"
-              :unit="activeUnit"
-            />
-          </div>
-        </div>
-
         <!-- Amount display -->
         <div class="col column items-center q-px-lg amount-area">
           <transition name="receive-content" mode="out-in">
@@ -260,7 +251,6 @@ import VueQrcode from "@chenfengyuan/vue-qrcode";
 import ChooseMint from "components/ChooseMint.vue";
 import NumericKeyboard from "components/NumericKeyboard.vue";
 import AmountInputComponent from "components/AmountInputComponent.vue";
-import OnchainDepositLimits from "components/OnchainDepositLimits.vue";
 import { useWalletStore } from "src/stores/wallet";
 import { useUiStore } from "src/stores/ui";
 import { useMintsStore } from "src/stores/mints";
@@ -281,7 +271,6 @@ export default defineComponent({
     ChooseMint,
     NumericKeyboard,
     AmountInputComponent,
-    OnchainDepositLimits,
     VueQrcode,
   },
   props: {},
